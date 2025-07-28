f9d633cc90c924c007754a4c0ee868c248d2d822
Fiayz <sifathhossain115@gmail.com>
2025-07-28 17:51:51 +0600

    Basic server running db config added

---
+++ config/db.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
+
+dotenv.config();
+
+export default sequelize = new Sequelize({
+  dialect: "postgres",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  logging: false,
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
