ea17bde8ede3d322b1b2ab676e104b7d4b3f6a4a
Fiayz <sifathhossain115@gmail.com>
2025-12-23 13:30:25 +0600

    Feat: db.ts updated to adjust Render postgre sql deployment

--- config/db.ts
+++ config/db.ts
@@ -1,21 +1,39 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import { Sequelize } from "sequelize";
 
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-const sequelize = new Sequelize({
-  dialect: "postgres",
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  logging: false,
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+const isProduction = process.env.NODE_ENV === "production";
+
+// dotenv.config();
+
+const sequelize =
+  isProduction && process.env.DATABASE_URL
+    ? new Sequelize(process.env.DATABASE_URL, {
+        dialect: "postgres",
+        logging: false,
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorised: false,
+          },
+        },
+      })
+    : new Sequelize({
+        dialect: "postgres",
+        host: process.env.DB_HOST,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        logging: false,
+        pool: {
+          max: 10,
+          min: 0,
+          acquire: 30000,
+          idle: 10000,
+        },
+      });
 
 export default sequelize;
--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import app from "./app";
 import { sequelize } from "./models";
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
