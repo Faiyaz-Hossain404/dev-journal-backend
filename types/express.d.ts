4c88b440a9d4cf762a57df71297ddaf140abf118
Fiayz <sifathhossain115@gmail.com>
2025-12-23 16:04:16 +0600

    Fix: dev dependecy; express v5 to v4 changed

--- types/express.d.ts
+++ types/express.d.ts
@@ -1,7 +1,7 @@
-import "express";
+// import "express";
 
-declare module "express-serve-static-core" {
-  interface Request {
-    user?: { id: string; name: string; email?: string };
-  }
-}
+// declare module "express-serve-static-core" {
+//   interface Request {
+//     user?: { id: string; name: string; email?: string };
+//   }
+// }
